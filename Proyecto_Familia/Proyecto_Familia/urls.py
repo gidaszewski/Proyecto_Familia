b19fbd4dcb59e34f44170bdd095a69b7c3e1284c
Franco Gidaszewski <116607624+gidaszewski@users.noreply.github.com>
2022-11-04 15:34:36 -0300

    Update urls.py

--- Proyecto_Familia/Proyecto_Familia/urls.py
+++ Proyecto_Familia/Proyecto_Familia/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from App1.views import Datos, infomama, infopadre, infohermana
+from App1.views import Datos, infomadre, infopadre, infohermana
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', Datos),
-    path('infomadre/', infomama),
+    path('infomadre/', infomadre),
     path('infopadre/', infopadre),
     path('infohermana/', infohermana),
 ]
